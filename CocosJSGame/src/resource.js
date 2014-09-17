b1d0be0df1c4df0a4995b3fc6a5d40055e00a18c
yangboz <YoungWelle@gmail.com>
2014-09-17 13:48:36 +0800

    ADDED:CocosJSGame example code base;

---
+++ CocosJSGame/src/resource.js
@@ -0,0 +1,15 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+    CloseNormal_png : "res/CloseNormal.png",
+    CloseSelected_png : "res/CloseSelected.png",
+    Arrow_png: "res/arrow.png",
+    Cat1_png: "res/cat1.png",
+    Catnorrisd_png: "res/catnorrisd.png",
+    End_png: "res/end.png",
+    Pg_png: "res/pg.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
